a64f3c8b21ec2ceaf1edd6507f8afab83b2a5618
Antony Kowalski <beheaded_96@mail.ru>
2024-07-09 18:04:35 +0300

    initial

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import { fileURLToPath } from 'url';
+import path, { dirname } from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	sassOptions: {
+		includePaths: [path.join(__dirname, 'styles')],
+	},
+	async redirects() {
+		return [];
+	},
+};
 
 export default nextConfig;
---
+++ src/shared/config/auth.ts
@@ -0,0 +1,16 @@
+import type { AuthOptions } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+
+export const authConfig: AuthOptions = {
+	providers: [
+		Credentials({
+			type: 'credentials',
+			name: 'auth by credentials',
+			credentials: {
+				email: { label: 'email', type: 'email', required: true },
+				password: { label: 'password', type: 'password', required: true },
+			},
+			async authorize(credentials, req) {},
+		}),
+	],
+};
